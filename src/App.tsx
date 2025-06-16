4be4403e5e1beae0441ee209fd69bfa65d84a4b4
javier <javierbk9@gmail.com>
2025-06-15 22:40:14 -0300

    feat: add the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { Button } from "./components/Button"
+import { Logo } from "./components/Logo"
 
 export const App = () => {
 
   return (
     <>
-      <h1>Changes</h1>
+      <header className="max-w-3xl mx-auto py-3 flex items-center justify-between">
+        <Button className="text-neutral-200 px-6 py-2 border border-gray-400 rounded-md" children={<Logo />} />
+
+        <div className="flex items-center gap-4">
+          <nav>
+            <ul className="flex gap-5">
+              <li><a className="text-neutral-800 dark:text-neutral-200" href="#Home">Home</a></li>
+              <li><a className="text-neutral-800 dark:text-neutral-200" href="#Experience">Experience</a></li>
+              <li><a className="text-neutral-800 dark:text-neutral-200" href="#Projects">Projects</a></li>
+              <li><a className="text-neutral-800 dark:text-neutral-200" href="#Contact">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
     </>
   )
 }
