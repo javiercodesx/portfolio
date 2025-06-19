3638860f31b8a9ea1f43a84ed2a4843b888ea6fc
javier <javierbk9@gmail.com>
2025-06-18 21:26:46 -0300

    add logo dark/white when the theme changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
 import { Button } from "./components/Button"
-// import { Logo } from "./components/Logo"
+import { Logo } from "./components/Logo"
 import { LogoDark } from "./components/LogoDark"
 import { ThemeToggle } from "./components/ToggleTheme"
 import { useTheme } from "./context/ThemeContext"
 
 export const App = () => {
   const { theme } = useTheme()
-
-  console.log(theme)
   return (
     <>
       <header className="max-w-3xl mx-auto p-2 md:px-0 flex items-center justify-between">
         <Button
           className="text-neutral-200 h-10 md:px-6 md:py-2 rounded-md"
-          children={<LogoDark />}
+          children={theme === "dark" ? <LogoDark/> : <Logo/>}
         />
 
         <div className="hidden md:flex items-center gap-4">
           <nav>
             <ul className="flex gap-5">
-              <li><a className="text-neutral-800 dark:text-neutral-200" href="#Home">Home</a></li>
-              <li><a className="text-neutral-800 dark:text-neutral-200" href="#Experience">Experience</a></li>
-              <li><a className="text-neutral-800 dark:text-neutral-200" href="#Projects">Projects</a></li>
-              <li><a className="text-neutral-800 dark:text-neutral-200" href="#Contact">Contact</a></li>
+              <li><a className="text-neutral-800 dark:text-neutral-200 text-md" href="#Home">Home</a></li>
+              <li><a className="text-neutral-800 dark:text-neutral-200 text-md" href="#Experience">Experience</a></li>
+              <li><a className="text-neutral-800 dark:text-neutral-200 text-md" href="#Projects">Projects</a></li>
+              <li><a className="text-neutral-800 dark:text-neutral-200 text-md" href="#Contact">Contact</a></li>
             </ul>
           </nav>
         </div>
