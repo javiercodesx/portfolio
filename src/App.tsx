48f6550a214c492ad775cb0bf5ac736aac60c47a
javier <javierbk9@gmail.com>
2025-06-28 16:55:59 -0300

    update app.tsx styles

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export const App = () => {
 
   return (
     <>
-      <header className="max-w-3xl mx-auto p-2 md:px-0 md:py-4 flex items-center justify-between">
+      <header className="max-w-4xl mx-auto p-2 md:px-0 md:py-4 flex items-center justify-between">
 
         {darkMode ? (
           <a href="/"><LogoDark /></a>
@@ -20,20 +20,20 @@ export const App = () => {
 
         <div className="hidden md:flex items-center gap-4">
           <nav>
-            <ul className="flex gap-5">
-              <li><a className="text-neutral-800 dark:text-neutral-200 text-md" href="#Home">Home</a></li>
-              <li><a className="text-neutral-800 dark:text-neutral-200 text-md" href="#Experience">Experience</a></li>
-              <li><a className="text-neutral-800 dark:text-neutral-200 text-md" href="#Projects">Projects</a></li>
-              <li><a className="text-neutral-800 dark:text-neutral-200 text-md" href="#Contact">Contact</a></li>
+            <ul className="flex gap-4">
+              <li><a className="text-neutral-800 dark:text-neutral-200 text-md px-3 py-2 hover:bg-neutral-700 rounded transition ease-in" href="#Home">Home</a></li>
+              <li><a className="text-neutral-800 dark:text-neutral-200 text-md px-3 py-2 hover:bg-neutral-700 rounded transition ease-in" href="#Experience">Experience</a></li>
+              <li><a className="text-neutral-800 dark:text-neutral-200 text-md px-3 py-2 hover:bg-neutral-700 rounded transition ease-in" href="#Projects">Projects</a></li>
+              <li><a className="text-neutral-800 dark:text-neutral-200 text-md px-3 py-2 hover:bg-neutral-700 rounded transition ease-in" href="#Contact">Contact</a></li>
             </ul>
           </nav>
         </div>
 
-        <div className="flex items-center gap-5">
+        <div className="flex items-center gap-3">
           <Button
-            className="text-neutral-200 px-3 py-1 cursor-pointer rounded-full border border-neutral-800 dark:border-neutral-300"
+            className="text-neutral-200 px-2 py-1.5 cursor-pointer rounded border border-neutral-800 dark:border-neutral-700 dark:hover:bg-neutral-700 transition ease-in"
             onClick={toggleTheme}
-            content={darkMode ? <IconMoon className='text-neutral-300 w-4 hover:scale-105' /> : <IconSun className='text-neutral-800 w-4 hover:scale-105' />}
+            content={darkMode ? <IconMoon className='text-neutral-300 w-5 ' /> : <IconSun className='text-neutral-800 w-5 ' />}
           />
 
           <HamburguerMenu />
@@ -41,8 +41,8 @@ export const App = () => {
 
       </header>
 
-      <section className="max-w-3xl px-2 md:px-0 mx-auto mt-[20dvh]">
-          <img src="/javier_gonzalez.jpg" alt="Javier González" className="h-24 md:h-48 rounded-xl md:rounded-4xl" />
+      <section className="max-w-4xl px-2 md:px-0 mx-auto mt-[20dvh]">
+          <img src="/javier_gonzalez.jpg" alt="Javier González" className="h-24 md:h-48 rounded-xl md:rounded-4xl bg-transparent" />
         <div className="flex items-center space-between">
           <h2 className="text-neutral-800 dark:text-neutral-300">Hola! soy Desarrollador Full Stack JavaScript</h2>
           <h3 className="text-neutral-800 dark:text-neutral-300">de Buenos Aires, Argentina.</h3>
