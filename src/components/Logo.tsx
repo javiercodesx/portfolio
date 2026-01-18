61c52084d83b42c9d089ebd1577d91861bee212e
Javier <javxdev@gmail.com>
2026-01-18 16:16:15 -0300

    add logo components

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from 'react'
+
+const Logo: () => ReactElement = (): ReactElement => {
+    return (
+        <img src="/logo.svg" alt="Javier Gonzalez"/>
+    )
+}
+
+export { Logo };
---
+++ src/components/LogoDark.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from 'react'
+
+const LogoDark: () => ReactElement = (): ReactElement => {
+    return (
+        <img src="/logo-dark.svg" alt="Javier Gonzalez"/>
+    )
+}
+
+export { LogoDark };
