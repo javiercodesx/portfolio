7f0d1e12bbefcbdfb904bb0b29965a6d5bee603f
Javier <javxdev@gmail.com>
2026-01-18 01:45:27 -0300

    remove unnecessary dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-//---------------------------------------------------------------------------------------------------------------------------------
-
-import type { ReactElement } from "react";
-import { AboutMe } from "./components/AboutMe";
-import { Experience } from "./components/Experience";
-import { Header } from "./components/Header";
-
-//---------------------------------------------------------------------------------------------------------------------------------
+import { ReactElement } from "react";
 
 const App: () => ReactElement = (): ReactElement => {
 
   return (
     <>
-      <Header />
-
-      <AboutMe />
-
-      <Experience />
+      <h1 className="h1">
+        Hey! I'm Javier. Full Stack Developer, I have a solid foundation in backend development, with a strong focus on Node.js — working with both Nest.js and Express.js. Specialized in building robust and scalable backend systems.
+      </h1>
     </>
   )
 }
